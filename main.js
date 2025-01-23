0b0aeb6f9e72cca5945068f41f71a74dbcedec28
Romer Flores <romer141016@gmail.com>
2025-01-23 19:52:04 -0400

    correcion Bug de Botton

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 function loadComponent(containerId, filePath, callback) {
     fetch(filePath)
         .then(response => response.text())
@@ -16,6 +18,8 @@ let sw=false
 let btn;
 let mobileMenu;
 
+let textoBanner;
+
 // Cargar el header y luego ejecutar el código que depende de él
 document.addEventListener('DOMContentLoaded', () => {
     loadComponent('header', 'pages/templates/header/header.html', () => {
@@ -24,10 +28,27 @@ document.addEventListener('DOMContentLoaded', () => {
         // Selecciona los elementos ya cargados
         btn = document.querySelector(".navbar__toogle-btn");
         mobileMenu = document.querySelector(".navbar__mobile-menu");
+        textoBanner = document.getElementById("textoBanner")
+
+
+        const hideResize= ()=>
+        {
+            mobileMenu.style.display="none";
+            sw=false
+        }
+
+        window.addEventListener("resize",hideResize);
+
 
 
         
-        
+        let datos=["Sabias que CRISTIANO RONALDO gano 35 copas hasta el dia de hoy","La criptografía es una técnica que se utiliza para proteger la información mediante la transformación de los datos en un formato ilegible para aquellos que no tienen la clave de descifrado","Sabias que demichelis gano 50 kilos despues del mundial","Messi gano 35 millones de dolares con la victoria del mundial","El perrito kesi kesi de los memes te desea un feliz aprendizaje"]
+
+        let puntero=Math.floor(Math.random() * 5);
+        console.log(puntero)
+        textoBanner.textContent=datos[puntero]
+
+
     });
 });
 
